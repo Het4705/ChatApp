2568a1a85a602a2ff5c8edf648915b227a0a2bf5
Het Jayeshbhai Patel <210303105289@paruluniversity.ac.in>
2024-06-08 02:15:50 +0530

    Added Active User count

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-// we are upgrading a connection from http to socket
-
 const express = require('express');
-const app=express();
+const app = express();
 const path = require('path');
-const {Server} =require("socket.io")
-
-// we can't attach socket io directly to express hence we need http module
-const http=require("http");
-const { Socket } = require('dgram');
-const server=http.createServer(app)  
-const io = new Server(server) // this will handle sockets
+const { Server } = require('socket.io');
+const http = require('http');
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(path.resolve("./public")));
-app.get("/",(req,res)=>{
-    return res.sendFile("/public/index.html")
-})
+app.get("/", (req, res) => {
+    return res.sendFile(path.resolve("./public/index.html")); // Use path.resolve for consistent file path
+});
 
-io.on("connection",(socket)=>{  //socket==client
-    console.log("new user")
-    socket.on("user-msg",message=>{
-        returnMessage=[message[0],message[1]];
-        io.emit("message",returnMessage);
-    }
-)})
+let activeUserCount = 0; // To store the number of active users
 
-server.listen(9000,()=>{
-    console.log(`server started at port 9000`)
-}) 
+io.on("connection", (socket) => {
+    const clientIPAddress = socket.handshake.address;
+    console.log(`New user connected from IP address: ${clientIPAddress}`);
+    activeUserCount++; // Increment the active user count
+
+    // Emit the updated active user count to all clients
+    io.emit('update-user-count', activeUserCount);
+
+    // Handle user messages
+    socket.on("user-msg", (message) => {
+        const returnMessage = [message[0], message[1]];
+        io.emit("message", returnMessage);
+    });
+
+    // Handle user disconnection
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+        activeUserCount--; // Decrement the active user count
+        io.emit('update-user-count', activeUserCount); // Broadcast the updated user count
+    });
+});
+
+server.listen(9000, () => {
+    console.log(`Server started at port 9000`);
+});
